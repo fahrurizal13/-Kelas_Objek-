45a601b7c2145d90e4d4722e4df69a6304e9e440
fahrurizal13 <70243921+fahrurizal13@users.noreply.github.com>
2020-08-26 08:31:46 +0800

    Update ObjekTanggal.java

--- ObjekTanggal.java
+++ ObjekTanggal.java
@@ -1,4 +1,8 @@
 import java.util.Date;
 
 public class ObjekTanggal{
+  public static void main(String[] args){
+    Date waktuSekarang = new Date();
+    System.out.println("Ini waktu sekarang"+waktuSekarang)
+  }
 }
