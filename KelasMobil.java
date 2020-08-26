55af6be065f7ae425efcf7d053586bafc4de9d45
fahrurizal13 <70243921+fahrurizal13@users.noreply.github.com>
2020-08-26 09:00:26 +0800

    Create KelasMobil.java

---
+++ KelasMobil.java
@@ -0,0 +1,15 @@
+class Mobil{
+    String warna;
+    int tahunProduksi;
+}
+public class KelasMobil {
+    public static void main(String[] args){
+        Mobil mobilku = new Mobil();
+        
+        mobilku.warna = "Merah";
+        mobilku.tahunProduksi = 2019;
+        
+        System.out.println("Warna : "+mobilku.warna);
+        System.out.println("Tahun : "+mobilku.tahunProduksi);
+    } 
+}
